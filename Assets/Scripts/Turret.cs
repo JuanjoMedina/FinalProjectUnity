ad1852b00688b7979f94dffc6b0508f1c3f48636
JuanjoMedina <juanjomedinamusellas@gmail.com>
2019-12-30 12:52:37 +0100

    Checked Updates

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : Character
+{
+    private float health;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        health = 200f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void damage(float damage)
+    {
+        health -= 20;
+    }
+}
