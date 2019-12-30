2073087ee91f864bee3e7a9930ef2491ab5d88fb
JuanjoMedina <juanjomedinamusellas@gmail.com>
2019-12-30 12:30:04 +0100

    Trying gitignore v2

---
+++ Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+    private Character character;
+    void Start()
+    {
+        character=(Character)transform.parent.GetComponent<MonoBehaviour>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string type = character.GetType().Name;
+        if (type.Equals("Player"))
+        {
+            if (collision.gameObject.tag == "Enemy")
+                character.damage(20f);
+        }
+        if (type.Equals("Enemy"))
+        {
+            if (collision.gameObject.tag == "Player")
+                character.damage(20f);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character
+{
+    private float health;
+    private bool dead;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        health = 80f;
+        dead = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        checkIfDead();
+    }
+    public override void damage(float damage)
+    {
+        health -= 20;
+    }
+    private void checkIfDead()
+    {
+        if (health <= 0)
+        {
+            dead = true;
+            Destroy(gameObject);
+        }
+
+    }
+}
