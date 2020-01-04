c79e700f5edf68b6eb37ea7d38e81994862f3951
JuanjoMedina <juanjomedinamusellas@gmail.com>
2020-01-04 02:55:23 +0100

    Reorganization and some error fixing

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player")|| collision.tag.Equals("Enemy"))
+        {
+            ((Character)collision.GetComponent<MonoBehaviour>()).Damage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player") || collision.tag.Equals("Enemy"))
+        {
+            ((Character)collision.GetComponent<MonoBehaviour>()).Damage(damage);
+        }
+    }
+}
+
