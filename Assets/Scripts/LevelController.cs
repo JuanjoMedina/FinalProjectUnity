7a72b6f602f15ebc02c6dd408c0eed035bdc2eca
Arnau <arnau27@pm.me>
2020-01-18 18:19:34 +0100

    Modified LevelController: Added idle, No loop w/delay & destroy gameObj

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -10,10 +10,13 @@ public class LevelController : MonoBehaviour
     public float xPosition;
     public float yPosition;
     public float zPosition;
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        //StartCoroutine(Coroutine());
     }
 
     // Update is called once per frame
@@ -21,13 +24,23 @@ public class LevelController : MonoBehaviour
     {
         
     }
-     void OnTriggerEnter2D(Collider2D collision)
+
+    IEnumerator delayCoroutine(Collider2D collision)
+    {
+        yield return new WaitForSeconds(2.0f);
+        collision.transform.position = new Vector3(xPosition, yPosition, zPosition);
+        //DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene(index);
+        Destroy(collision.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = new Vector3(xPosition, yPosition, zPosition);
-            DontDestroyOnLoad(this.gameObject);
-            SceneManager.LoadScene(index);
+            animator.Play("exit");
+            StartCoroutine(delayCoroutine(collision));
+
             //SceneManager.LoadScene(levelName);
         }
     }
